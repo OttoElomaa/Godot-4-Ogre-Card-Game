3267d21c2f8d1a6e06e430efe02012c7dbef436c
Otto Elomaa <otto.elomaa99@gmail.com>
2025-08-12 23:30:54 +0300

    Fixed the remaining dependencies for Intro scene and its components. Integrated it to MainMenu screen for now.

--- GameScenes/intro.gd
+++ GameScenes/intro.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
+
+
 func _process(delta):
-	if Input.is_anything_pressed():
-		get_tree().change_scene_to_file("res://GameScenes/MainMenu.tscn")
+	pass
+	#if Input.is_anything_pressed():
+		#get_tree().change_scene_to_file("res://GameScenes/MainMenu.tscn")
 		
